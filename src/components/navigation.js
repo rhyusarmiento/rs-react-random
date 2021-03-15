f8addb8dc5c9ff68ff20c6b5b8bfbc8cdb49260f
Rhyu Sarmiento <rhyu.sar.15@gmail.com>
2021-03-15 09:20:20 -0600

    add sass to project and styles

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,19 +4,17 @@ import { NavLink } from "react-router-dom";
 export default function NavigationComponent() {
   return (
     <div className="nav-wrapper">
-      <div className="nav-content">
-        <NavLink exact to="/">
-          Home
-        </NavLink>
-        <NavLink to="/hamburger">Hamburger</NavLink>
-        <NavLink to="/show-hide">Show Hide</NavLink>
-        <NavLink to="/toggle">Toggle</NavLink>
-        <NavLink to="/counter">Counter</NavLink>
-        <NavLink to="/font-sizer">Font Sizer</NavLink>
-        <NavLink to="/align">Align</NavLink>
-        <NavLink to="/color">Color</NavLink>
-        <NavLink to="/clock">Clock</NavLink>
-      </div>
+      <NavLink exact to="/">
+        Home
+      </NavLink>
+      <NavLink to="/hamburger">Hamburger</NavLink>
+      <NavLink to="/show-hide">Show Hide</NavLink>
+      <NavLink to="/toggle">Toggle</NavLink>
+      <NavLink to="/counter">Counter</NavLink>
+      <NavLink to="/font-sizer">Font Sizer</NavLink>
+      <NavLink to="/align">Align</NavLink>
+      <NavLink to="/color">Color</NavLink>
+      <NavLink to="/clock">Clock</NavLink>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './components/App'
+
+import './style/main.scss'
 
 ReactDOM.render(
   <React.StrictMode>
